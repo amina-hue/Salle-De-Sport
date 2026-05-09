aea13d21c54670aa44aa1f9ccf969497592a1b8e
tintin <tinhinane.belarbi@se.univ-bejaia.dz>
2026-05-09 21:07:19 +0100

    test main

--- jest.setup.js
+++ jest.setup.js
@@ -1,3 +1,6 @@
 const { TextEncoder, TextDecoder } = require('util');
 global.TextEncoder = TextEncoder;
 global.TextDecoder = TextDecoder;
+
+global.MAIN_WINDOW_WEBPACK_ENTRY = 'http://localhost:3000';
+global.MAIN_WINDOW_PRELOAD_WEBPACK_ENTRY = '/preload.js';
