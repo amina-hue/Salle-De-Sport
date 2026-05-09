93d52b76c0bde94283ca5a2828baa4d1fd5ae154
tintin <tinhinane.belarbi@se.univ-bejaia.dz>
2026-05-09 17:48:47 +0100

    test

--- jest.setup.js
+++ jest.setup.js
@@ -1,9 +1,3 @@
-
-import '@testing-library/jest-dom';
-
-// Fix pour react-router-dom (TextEncoder non défini dans jsdom)
-
 const { TextEncoder, TextDecoder } = require('util');
 global.TextEncoder = TextEncoder;
 global.TextDecoder = TextDecoder;
-
